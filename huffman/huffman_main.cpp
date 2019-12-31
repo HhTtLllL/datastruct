3800d4a3e2748fe70fc26c27b363e377e880c1be
HhTtLllL <1430249706@qq.com>
2019-12-31 13:15:22 +0800

    基本完成

---
+++ huffman/huffman_main.cpp
@@ -0,0 +1,36 @@
+//##################################################################
+// File Name: huffman_main.cpp
+// File Effect: 
+// Author: tttt
+// mail: tttt@xiyoulinux.org
+// Created Time: 2019年12月30日 星期一 23时04分59秒
+//=============================================================
+//!/usr/bin/python
+
+#include "./huffman.h"
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int select;
+	do
+	{
+		printf("1  压缩\n");
+		printf("2  解压缩\n");
+		printf("3  退出\n");
+		printf("输入选项:");
+		scanf("%d",&select);
+		fflush;
+		switch(select)
+		{
+			case 1:
+				commpression();break;
+			case 2:
+				Decode();break;
+		}
+
+	}while(select != 3);
+
+	return 0;
+}
